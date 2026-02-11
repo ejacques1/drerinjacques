76a53138ecac1a7f4756ead86b64007cf770bf8d
Dr. Erin Jacques <drerintj@gmail.com>
2026-02-10 19:25:16 -0500

    Update subscriber API to use tagId instead of tags
    Replaced 'tags' array with 'tagId' for subscriber.

--- api/subscribe.js
+++ api/subscribe.js
@@ -78,7 +78,7 @@ export default async function handler(req, res) {
         'X-API-Key': apiKey
       },
       body: JSON.stringify({
-        tags: ['Website-Subscriber']
+        tagId: '1864099'
       })
     });
 
